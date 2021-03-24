96fb89046a37cbc126e0af5af4200747103fa4de
Johndoe <56298669+togane@users.noreply.github.com>
2021-03-25 06:19:11 +0900

    add race trac model

---
+++ keiba_machine_learning/models/race.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class RaceTrac(Enum):
+    HOKKAIDO = 1
+    HAKODATE = 2
+    FUKUSHIMA = 3
+    NIGATA = 4
+    TOKYO = 5
+    NAKAYAMA = 6
+    CHUKYO = 7
+    KYOTO = 8
+    HANSHIN = 9
+    KOKURA = 10
