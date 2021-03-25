94c7fa084c916aba3111d4d4e1cd01e776b91f2f
Johndoe <56298669+togane@users.noreply.github.com>
2021-03-25 19:16:32 +0900

    implement race models

---
+++ keiba_machine_learning/models/tests/test_race.py
@@ -0,0 +1,5 @@
+from keiba_machine_learning.models.race import RaceTrac, Race
+
+def test_identifier():
+    race = Race(year=2020, race_track=RaceTrac.HOKKAIDO, series_number=1, day_number=1, race_number=1)
+    assert race.id == 202001010101
